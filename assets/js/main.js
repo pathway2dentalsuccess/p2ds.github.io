a15c85be4a1d06fdfe9e3097d866cbf74ba7e009
pathwaytodentalsuccess <vishrutha06@gmail.com>
2022-02-15 22:09:14 -0500

    contactphp

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,39 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+//Contact Form in PHP - added in
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area span");
+
+form.onsubmit = (e)=>{
+  e.preventDefault();
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+  statusTxt.innerText = "Sending your message...";
+  form.classList.add("disabled");
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "message.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState == 4 && xhr.status == 200){
+      let response = xhr.response;
+      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
+        statusTxt.style.color = "red";
+      }
+	  else{
+        form.reset();
+        setTimeout(()=>{
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
+    }
+  }
+  let formData = new FormData(form);
+  xhr.send(formData);
+}
+
 (function($) {
 
 	var	$window = $(window),
