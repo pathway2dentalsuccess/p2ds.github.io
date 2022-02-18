d13f3e6e8456ce891c7b3e021bd94710a9f5b8bc
pathwaytodentalsuccess <vishrutha06@gmail.com>
2022-02-17 21:41:54 -0500

    contactme

---
+++ assets/contact-handler.php
@@ -0,0 +1,17 @@
+<?php
+    $name = $_Post['name'];
+    $visitor_email = $Post['email'];
+    $message = $_POST['message']
+
+    $email_from = 'pathwaytodentalsuccess@gmail.com'
+    $email_subject = 'New Message From Website'
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+    $to = "pathwaytodentalsuccess@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: contactMe.html");
+?>
