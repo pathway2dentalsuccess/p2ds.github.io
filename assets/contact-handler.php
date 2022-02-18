233d2ab8162380ebc8b9edce5c464686ca5fa9d8
pathwaytodentalsuccess <vishrutha06@gmail.com>
2022-02-17 22:29:49 -0500

    contactform

--- assets/contact-handler.php
+++ assets/contact-handler.php
@@ -1,17 +1,34 @@
 <?php
     $name = $_POST['name'];
-    $visitor_email = $POST['email'];
-    $message = $_POST['message']
+    $user_email = $POST['email'];
+    $message = $_POST['message'];
 
-    $email_from = 'pathwaytodentalsuccess@gmail.com'
-    $email_subject = "New Message From Website"
+    $email_from = 'pathwaytodentalsuccess@gmail.com';
+    $email_subject = "New Message From Website";
     $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
+                    "User Email: $user_email.\n".
                         "User Message: $message.\n";
 
     $to = "pathwaytodentalsuccess@gmail.com";
     $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
-    mail($to,$email_subject,$email_body,$headers);
+    $headers .= "Reply-To: $user_email \r\n";
+
     header("Location: contactMe.html");
+
+    $secretKey = "6LeVtYYeAAAAAEN5hAd88EhmPOqz0WKh_5X1_Ip7";
+    $responseKey = $_POST['g-recaptcha-response'];
+    $UserIP = $_SERVER['REMOTE_ADDR'];
+    $url = "https://google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$UserIP";
+
+    $response = file_get_contents($url);
+    $response = json_decode($response);
+
+    if ($response -> success) {
+        mail($to,$email_subject,$email_body,$headers);
+        echo "Message sent Successfully";
+    }
+    else {
+        echo "<span>Invalid Captcha. Please try again.</span>";
+    }
+
 ?>
